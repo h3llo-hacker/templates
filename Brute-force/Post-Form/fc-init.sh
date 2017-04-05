921bb56d5fb55919826a77ea080c6c94027a7ca8
wrfly <mr.wrfly@gmail.com>
2017-04-05 22:15:25 +0800

    modify

--- Brute-force/Post-Form/fc-init.sh
+++ Brute-force/Post-Form/fc-init.sh
@@ -5,14 +5,15 @@ if [[ "$FLAG" == "" ]]; then
 fi
 
 ENV_FILE="/app/env.php"
+echo "<?php" > $ENV_FILE
 
 # putenv('APPLICATION_ENV=staging');
-
 # FLAG_ENV=$(env | grep FLAG)
 echo "putenv(\"$(env | grep FLAG)\");" >> $ENV_FILE
 
 NUM=$(date +%s | tail -c 4)
 PASS=$(head -n $NUM /top_1000.txt| tail -1)
+echo "Password: $PASS"
 export PASSWD=$PASS
 
 echo "putenv(\"$(env | grep PASSWD)\");" >> $ENV_FILE
