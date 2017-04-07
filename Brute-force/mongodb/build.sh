ba58c43084c19c027fc20ee5e8f99049596d2a97
wrfly <mr.wrfly@gmail.com>
2017-04-07 16:54:15 +0800

    rename ftp

--- Brute-force/mongodb/build.sh
+++ Brute-force/mongodb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t wrfly/brute-force_mongodb:latest .
+docker build -t wrfly/brute_force-mongodb:latest .
 
-docker run --rm -ti -p 27117:27017 -e FLAG=flag{hello} wrfly/brute-force_mongodb:latest
+docker run --rm -ti -p 27117:27017 -e FLAG=flag{hello} wrfly/brute_force-mongodb:latest
