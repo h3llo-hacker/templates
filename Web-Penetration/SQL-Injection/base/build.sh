efe14944fad6f9c7e77ec7c6c3e767b6bd7d2ec1
wrfly <mr.wrfly@gmail.com>
2017-04-09 16:46:50 +0800

    rename image tag

--- Web-Penetration/SQL-Injection/base/build.sh
+++ Web-Penetration/SQL-Injection/base/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wrfly/web_pt-sql_inject:latest . && \
-docker run --rm -ti -e FLAG=hhhhhh -p 8080:80 wrfly/web_pt-sql_inject:latest
+docker build -t wrfly/web_pt-sql_inject:base . && \
+docker run --rm -ti -e FLAG=hhhhhh -p 8080:80 wrfly/web_pt-sql_inject:base
