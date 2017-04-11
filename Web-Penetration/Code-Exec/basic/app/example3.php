bc8d281894f154cb98c670aa3395e48abfa6cb6d
wrfly <mr.wrfly@gmail.com>
2017-04-11 17:37:46 +0800

    PHP code exec (done)

--- Web-Penetration/Code-Exec/basic/app/example3.php
+++
@@ -1,8 +0,0 @@
-<?php require_once "header.php";?>
-<?php
-echo preg_replace($_GET["pattern"], $_GET["new"], $_GET["base"]);
-
-?>
-
-
-<?php require_once "footer.php";?>
--- Web-Penetration/XSS-Test/example/app/footer.php
+++ Web-Penetration/XSS-Test/example/app/footer.php
@@ -1,8 +1,14 @@
+<br>
+<br>
       <footer>
-        <p>&copy; <a href="https://pentesterlab.com">PentesterLab</a> 2013 | Dockerized by <a href="https://github.com/wrfly/" target="_blank">wrfly</a> </p>
+        <p>
+        	&copy; <a href="https://pentesterlab.com">PentesterLab</a> 2013
+        	| &copy; <a href="https://h3llo.cn">H3LLO </a> 2017
+        	| Dockerized by <a href="https://github.com/wrfly/" target="_blank">wrfly</a>  </p>
       </footer>
 
     </div> <!-- /container -->
+
 
   </body>
 </html>
