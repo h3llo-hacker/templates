60f2db777d77131711d6f7a2bc7256ea55f6cccc
wrfly <mr.wrfly@gmail.com>
2017-04-17 16:17:46 +0800

    rename

--- Web-Penetration/File-Include/level_0/build.sh
+++ Web-Penetration/File-Include/level_0/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wrfly/web_pt-fileincl:level_0 . && \
-docker run --rm -ti -e FLAG=hhhhhh -p 8080:80 wrfly/web_pt-fileincl:level_0
+docker build -t wrfly/web_pt-fileinclude:level_0 . && \
+docker run --rm -ti -e FLAG=hhhhhh -p 8080:80 wrfly/web_pt-fileinclude:level_0
